92e4c05fe4a6cc8968bca33196b63e0d54eb96c3
Mehak42 <tiwmy001@mymail.unisa.edu.au>
2025-11-11 19:51:37 +1030

    Add file description

--- animal.py
+++ animal.py
@@ -1,8 +1,8 @@
 '''
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+File: animal.py
+Description: Defines animal class used in the zoo management system.
+Author: Mehakdeep Kaur Tiwana
+ID: 110397073
+Username: tiwmy001
 This is my own work as defined by the University's Academic Integrity Policy.
 ''
