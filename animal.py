3cdb178699bf9343ca79f454633fbba8136addfd
Mehak42 <tiwmy001@mymail.unisa.edu.au>
2025-11-11 19:55:46 +1030

    Add constructors and initializers in Animal Class

--- animal.py
+++ animal.py
@@ -5,4 +5,14 @@ Author: Mehakdeep Kaur Tiwana
 ID: 110397073
 Username: tiwmy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Animal:
+    def __init__(self, name, species, age, dietary_needs, animal_health):
+        self.__name = name
+        self.__species = species
+        self.__age = age
+        self.__dietary_needs = dietary_needs
+        self.__animal_health = animal_health
+
+
