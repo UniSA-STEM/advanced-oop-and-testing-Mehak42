b60227e1f04327dde7d0c6fe5d4e039f9cb50ac1
Mehak42 <tiwmy001@mymail.unisa.edu.au>
2025-11-19 22:48:10 +1030

    Add constructors and initializers in enclosure.py

--- enclosure.py
+++ enclosure.py
@@ -1,8 +1,20 @@
 '''
 File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Description: Defines Enclosure class used to house animals in the zoo.
+Author: Mehakdeep Kaur Tiwana
+ID: 110397073
+Username: tiwmy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Enclosure:
+    def __init__(self, name, size, environment_type, cleanliness_level):
+        self.__name = name
+        self.__size = size
+        self.__environment_type = environment_type
+        self.__cleanliness_level = cleanliness_level
+        self.__animals = []
+
+    def get_name(self):
+        return self.__name
+
