de21bd6449efe504fb902366a37b12bc2daab421
Mehak42 <tiwmy001@mymail.unisa.edu.au>
2025-11-21 22:00:29 +1030

    Test Animal creation in main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 '''
 File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Description: This is the main class where i will be testing all 3 classes together
+Author: Mehakdeep kaur Tiwana
+ID: 110397073
+Username: tiwmy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+from animal import Animal, Mammal, Reptile, Bird, HealthIssue
+from enclosure import Enclosure
+
+print("\n--- Testing Animal Creation ---")
+leo = Mammal("Leo", "Lion", 5, "Meat", "Healthy", "Golden Fur")
+print(leo)
+leo.make_sound()
+leo.eat()
+leo.sleep()
+
