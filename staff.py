4ed9f68ad3ed9db814c4c1890efc4c1b6d14eef8
Mehak42 <tiwmy001@mymail.unisa.edu.au>
2025-11-22 16:42:34 +1030

    Add assign_animal function to Staff class to assign animals to the staff

--- staff.py
+++ staff.py
@@ -12,12 +12,31 @@ from enclosure import Enclosure
 
 class Staff:
     def __init__(self, name, role):
-        self.name = name
-        self.role = role
+        self.__name = name
+        self.__role = role
         self.__assigned_animals = []
         self.__assigned_enclosures = []
 
     def get_name(self):
+        return self.__name
+    def get_role(self):
+        return self.__role
+    def get_assigned_animals(self):
+        return self.__assigned_animals
+    def get_assigned_enclosures(self):
+        return self.__assigned_enclosures
+
+    def assign_animal(self, animal):
+        if isinstance(animal, Animal):
+            self.__assigned_animals.append(animal)
+            print(f"{self.__name} has been assigned to {animal.name}")
+        else:
+            print("No animal assigned")
+
+
+
+
+
 
 
 
